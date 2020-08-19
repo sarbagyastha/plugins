eb545af54b891b651acfca1180ac1a8958fa6c40
Sarbagya Dhaubanjar <github@sarbagyastha.com.np>
2020-08-19 13:06:45 +0545

    Update Camera.java

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -320,6 +320,8 @@ public class Camera {
               cameraCaptureSession = session;
               captureRequestBuilder.set(
                   CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+              captureRequestBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_OFF);
+              captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON);
               cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
               if (onSuccessCallback != null) {
                 onSuccessCallback.run();
