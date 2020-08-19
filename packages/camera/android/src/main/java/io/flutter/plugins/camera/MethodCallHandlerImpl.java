de33acd08b8d0f57d337d691284c2273d0860df8
Sarbagya Dhaubanjar <github@sarbagyastha.com.np>
2020-08-19 16:47:51 +0545

    Update MethodCallHandlerImpl.java

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/MethodCallHandlerImpl.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/MethodCallHandlerImpl.java
@@ -125,7 +125,7 @@ final class MethodCallHandlerImpl implements MethodChannel.MethodCallHandler {
         }
       case "switchFlash":
         {
-          camera.switchFlash(call.argument("on", result));
+          camera.switchFlash(call.argument("on"), result);
           break;
         }
       case "dispose":
